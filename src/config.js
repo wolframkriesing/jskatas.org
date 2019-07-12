d4ba1cefbd1ef5d96a52dea412fac554546cfaa3
wolframkriesing <wolfram+github@kriesing.de>
2019-07-12 19:18:51 +0200

    Use the ENV vars.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-const DOMAIN = process.env['KATAS_URL'] || 'https://katas.tddbin.com';
+const KATAS_URL = process.env.KATAS_URL || 'https://katas.tddbin.com';
 export const metadataUrls = {
-  es1: `${DOMAIN}/katas/es1/language/__grouped__.json`,
-  es6: `${DOMAIN}/katas/es6/language/__grouped__.json`,
-  es8: `${DOMAIN}/katas/es8/language/__grouped__.json`,
-  hamjest: `${DOMAIN}/katas/libraries/hamjest/__grouped__.json`,
+  es1: `${KATAS_URL}/es1/language/__grouped__.json`,
+  es6: `${KATAS_URL}/es6/language/__grouped__.json`,
+  es8: `${KATAS_URL}/es8/language/__grouped__.json`,
+  hamjest: `${KATAS_URL}/libraries/hamjest/__grouped__.json`,
 };
-export const FLAT_METADATA_URL = `${DOMAIN}/katas/es6/language/__all__.json`;
+export const FLAT_METADATA_URL = `${KATAS_URL}/es6/language/__all__.json`;
--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,10 @@ import {render as preactRender} from 'preact-render-to-string';
 import {loadViaNode} from './http-get.js';
 import Page from '../components/page.js';
 import {loadKataBundles} from '../pagedata.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const renderOnServer = (kataBundles) => 
+const renderOnServer = (kataBundles) =>
   preactRender(<Page kataBundles={kataBundles}/>);
 
 export const render = async () => {
