a1561b79f178a76a77af6e4c4517a955317b19e7
wolframkriesing <wolfram+github@kriesing.de>
2019-07-12 19:47:08 +0200

    Better name.

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,12 @@
 import './configure-env.js';
 import {h} from 'preact';
-import {render as preactRender} from 'preact-render-to-string';
+import {render as renderToString} from 'preact-render-to-string';
 import {loadViaNode} from './http-get.js';
 import Page from '../components/page.js';
 import {loadKataBundles} from '../pagedata.js';
 
 const renderOnServer = (kataBundles) =>
-  preactRender(<Page kataBundles={kataBundles}/>);
+  renderToString(<Page kataBundles={kataBundles}/>);
 
 export const render = async () => {
   const kataBundles = await loadKataBundles({fetch: loadViaNode});
