b6773848e18dc7993b330f9b39245b7aa1e68346
wolframkriesing <wolfram+github@kriesing.de>
2019-07-12 20:36:51 +0200

    Show plausible badge.

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,16 @@
 import {h, Component} from 'preact';
 
+const PLAUSIBLE_URL = 'https://plausible.io/jskatas.org';
+
 export default class HeaderComponent extends Component {
   render() {
     return (
       <header>
         <h1>JavaScript Katas</h1>
         <p>Learn JavaScript (the language and more) by doing it. Fix failing tests. Keep all learnings.</p>
+        <a href={PLAUSIBLE_URL} className="icon plausible" title="Public traffic stats">
+          <img src="./plausible_badge.svg" />
+        </a>
       </header>
     );
   }
