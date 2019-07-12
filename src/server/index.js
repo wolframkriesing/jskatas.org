5923c8a699570f3b535a8bf7fba8463775920dd2
wolframkriesing <wolfram+github@kriesing.de>
2019-07-12 19:41:33 +0200

    Configure the env before using it :).

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,9 @@
+import './configure-env.js';
 import {h} from 'preact';
 import {render as preactRender} from 'preact-render-to-string';
 import {loadViaNode} from './http-get.js';
 import Page from '../components/page.js';
 import {loadKataBundles} from '../pagedata.js';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const renderOnServer = (kataBundles) =>
   preactRender(<Page kataBundles={kataBundles}/>);
