5101421fd9dbffd96e64f5ebea13b9d3bc9f023f
wolframkriesing <wolfram+github@kriesing.de>
2019-07-16 00:19:21 +0200

    Render kata name with embedded <code>.

--- src/components/kata-name.js
+++ src/components/kata-name.js
@@ -21,19 +21,41 @@ class KataName extends HTMLElement {
   }
   connectedCallback() {
     const slots = this.shadowRoot.querySelectorAll('slot');
-    slots[0].addEventListener('slotchange', (e) => {
+    const f = (e) => {
+      slots[0].removeEventListener('slotchange', f); // remove right away, so our changes dont cause eternal loops
       const nodes = slots[0].assignedNodes();
       const isComponentConnected = nodes.length > 0;
       if (isComponentConnected) {
         const node = nodes[0];
         const text = node.textContent;
-        const newText = text
-          .replace('`', '<code>')
-          .replace('`', '</code>');
-console.log("replaced text= ", newText);
-        node.textContent = newText;
+
+        if (!text.includes('`')) {
+          return;
+        }
+
+        if (text.startsWith('`') && text.endsWith('`')) {
+          const code = document.createElement('code');
+          code.innerText = text.replace(/`/g, '');
+          nodes[0].parentNode.appendChild(code);
+          nodes[0].parentNode.removeChild(nodes[0]);
+          return;
+        }
+        const pieces = text.split('`');
+
+        const text1 = document.createTextNode(pieces[0]);
+        nodes[0].parentNode.appendChild(text1);
+
+        const code = document.createElement('code');
+        code.innerText = pieces[1];
+        nodes[0].parentNode.appendChild(code);
+
+        const text2 = document.createTextNode(pieces.slice(2).join(''));
+        nodes[0].parentNode.appendChild(text2);
+
+        nodes[0].parentNode.removeChild(nodes[0]);
       }
-    });
+    };
+    slots[0].addEventListener('slotchange', f);
   }
   disconnectedCallback() {
   }
