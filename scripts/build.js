c712b49a9f69df55a364b892856b7bb86bc9f676
wolframkriesing <wolfram+github@kriesing.de>
2019-08-04 00:03:16 +0200

    Extract paths.

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
-const ssr = fs.readFileSync(
-  path.join(__dirname, '../tmp/ssr-result.html'),
-  'utf8',
-);
-const indexHtml = fs.readFileSync(
-  path.join(__dirname, '../src/index.html'),
-  'utf8',
-);
+const DIST_INDEX_HTML = path.join(__dirname, '../dist/index.html');
+const SSR_RENDERED = path.join(__dirname, '../tmp/ssr-result.html');
+const SRC_INDEX_HTML = path.join(__dirname, '../src/index.html');
+
+const ssr = fs.readFileSync(SSR_RENDERED, 'utf8');
+const indexHtml = fs.readFileSync(SRC_INDEX_HTML, 'utf8');
 
 const openingHtml = '<div id="app">';
 const closingHtml = '</div>';
@@ -17,4 +15,4 @@ const prerenderedHtml = indexHtml.replace(
   openingHtml + closingHtml,
   openingHtml + ssr + closingHtml,
 );
-fs.writeFileSync(path.join(__dirname, '../dist/index.html'), prerenderedHtml);
+fs.writeFileSync(DIST_INDEX_HTML, prerenderedHtml);
