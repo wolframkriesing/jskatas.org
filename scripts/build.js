5f45fd3508444ad3f009c60df93446724887cc4d
wolframkriesing <wolfram+github@kriesing.de>
2019-08-03 23:20:52 +0200

    Deploy into dist.

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const ssr = fs.readFileSync(path.join(__dirname, '../dist/ssr-result.html'), 'utf8');
+const ssr = fs.readFileSync(path.join(__dirname, '../tmp/ssr-result.html'), 'utf8');
 const indexHtml = fs.readFileSync(path.join(__dirname, '../src/index.html'), 'utf8');
 
 const openingHtml = '<div id="app">';
