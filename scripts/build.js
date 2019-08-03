a85615d602c083c0396aa3486c7c2fa6be6baaa2
wolframkriesing <wolfram+github@kriesing.de>
2019-08-03 23:11:08 +0200

    Build the SSRed page into dist.

---
+++ scripts/build.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
+const ssr = fs.readFileSync(path.join(__dirname, '../dist/ssr-result.html'), 'utf8');
+const indexHtml = fs.readFileSync(path.join(__dirname, '../src/index.html'), 'utf8');
+
+const openingHtml = '<div id="app">';
+const closingHtml = '</div>';
+
+const prerenderedHtml = indexHtml.replace(openingHtml+closingHtml, openingHtml+ssr+closingHtml);
+fs.writeFileSync(path.join(__dirname, '../dist/index.html'), prerenderedHtml);
