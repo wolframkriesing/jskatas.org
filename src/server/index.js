fb5bae06be75c8b55b68ef3dc0c5edd1ebe1c668
wolframkriesing <wolfram+github@kriesing.de>
2019-08-03 22:48:35 +0200

    Make the server side rendering output the rendered HTML.

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,11 @@
-import {render} from '../lit-html.js';
 import {loadViaAjax} from './ajax.js';
 import {Page} from '../components/page.js';
 import {loadKataBundles} from '../pagedata.js';
 
-const renderInBrowser = kataBundles => {
-  const container = {};
-  render(Page({kataBundles}), container);
-};
+const render = kataBundles => Page({kataBundles});
 
 const loadAllKatas = async () => {
   const kataBundles = await loadKataBundles({fetch: loadViaAjax});
-  renderInBrowser(kataBundles);
+  console.log(render(kataBundles));
 };
 loadAllKatas();
