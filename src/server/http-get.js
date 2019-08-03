651bc52102bc9b581d1d90e320bbeed10e49b4b1
wolframkriesing <wolfram+github@kriesing.de>
2019-08-03 15:13:13 +0200

    Starting with the SSR ...

--- src/server/http-get.js
+++
@@ -1,3 +0,0 @@
-import fetch from 'isomorphic-fetch';
-
-export const loadViaNode = url => fetch(url).then(response => response.json());
--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,15 @@
-import './configure-env.js';
-import {render} from 'lit-html';
-import {loadViaNode} from './http-get.js';
+import {render} from '../lit-html.js';
+import {loadViaAjax} from './ajax.js';
 import {Page} from '../components/page.js';
 import {loadKataBundles} from '../pagedata.js';
 
-// const renderOnServer = kataBundles =>
-//   renderToString(<Page kataBundles={kataBundles} />);
-//
-// export const render = async () => {
-//   const kataBundles = await loadKataBundles({fetch: loadViaNode});
-//   return renderOnServer(kataBundles);
-// };
+const renderInBrowser = kataBundles => {
+  const container = {};
+  render(Page({kataBundles}), container);
+};
+
+const loadAllKatas = async () => {
+  const kataBundles = await loadKataBundles({fetch: loadViaAjax});
+  renderInBrowser(kataBundles);
+};
+loadAllKatas();
