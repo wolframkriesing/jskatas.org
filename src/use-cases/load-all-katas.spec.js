c790c2b76438301e65bd95470d1af31685cffbfc
wolframkriesing <wolfram+github@kriesing.de>
2019-10-30 18:58:56 +0100

    Sort tests anew.

--- src/use-cases/load-all-katas.spec.js
+++ src/use-cases/load-all-katas.spec.js
@@ -8,14 +8,6 @@ import {loadViaAjax} from '../server/ajax.js';
 const loadAllKatas = loadAllKatasConstructor({fetch: loadViaAjax});
 
 describe('Load all katas', () => {
-  it('a kata contains the `bundleName` (e.g. `es6/language`)', async () => {
-    const katas = await loadAllKatas();
-
-    const firstKata = katas[0];
-    assert(Reflect.has(firstKata, 'bundleName'));
-    const lastKata = katas[katas.length - 1];
-    assert(Reflect.has(lastKata, 'bundleName'));
-  });
   it('loads all configured kata bundles', async () => {
     let urlsCalled = [];
     const fakeFetch = async (url) => {
@@ -30,5 +22,13 @@ describe('Load all katas', () => {
     bundleConfigs.withEach(config =>
       assert(urlsCalled.includes(config.sourceUrl), `Bundle "${config.bundleName}" was not loaded.`)
     );
+  });
+  it('a kata contains the `bundleName` (e.g. `es6/language`)', async () => {
+    const katas = await loadAllKatas();
+
+    const firstKata = katas[0];
+    assert(Reflect.has(firstKata, 'bundleName'));
+    const lastKata = katas[katas.length - 1];
+    assert(Reflect.has(lastKata, 'bundleName'));
   });
 });
