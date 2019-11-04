f1189af8c550e8130643d99a9859d603fd289c75
wkriesing <wolfram.kriesing@holidaycheck.com>
2019-11-04 22:39:13 +0100

    Allow forcing offline by adding ?offline to any URL.

--- src/browser/lit-html.js
+++ src/browser/lit-html.js
@@ -1,6 +1,12 @@
 const LITHTML_OFFLINE_PATH = '/node_modules/lit-html/lit-html.js';
 const LITHTML_ONLINE_PATH = 'https://unpkg.com/lit-html?module';
-const LITHTML_PATH = navigator.onLine ? LITHTML_ONLINE_PATH : LITHTML_OFFLINE_PATH;
+const isOnline = navigator.onLine;
+const forceOffline = new URLSearchParams(location.search).has('offline');
+const LITHTML_PATH = (
+    forceOffline || !isOnline 
+    ? LITHTML_OFFLINE_PATH
+    : LITHTML_ONLINE_PATH 
+);
 
 export const loadLitHtml = async () => {
   window.litHtml = await import(LITHTML_PATH);
