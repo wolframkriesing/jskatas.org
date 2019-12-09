8c15ce3d0b13fa8b47e48af49a22ba79e6dc06ac
wolframkriesing <wolfram+github@kriesing.de>
2019-12-09 23:34:59 +0100

    Fix forgotten variable name.

--- src/test/katabundle-spec.js
+++ src/test/katabundle-spec.js
@@ -26,25 +26,25 @@ describe('KataBundle', () => {
   });
 });
 describe('sort kata groups', () => {
-  const kataGroups = new KataBundle();
-  kataGroups.addGroup(
+  const kataBundle = new KataBundle();
+  kataBundle.addGroup(
     KataGroup.fromRawKataItems('group with 1 kata', [Kata.withId(0)]),
   );
-  kataGroups.addGroup(
+  kataBundle.addGroup(
     KataGroup.fromRawKataItems('group with 2 katas', [
       Kata.withId(1),
       Kata.withId('21'),
     ]),
   );
-  kataGroups.addGroup(
+  kataBundle.addGroup(
     KataGroup.fromRawKataItems('group with newest kata', [
       Kata.withoutPublishDate(),
       Kata.withPublishDate('2015-03-13T07:55:00.000Z'),
     ]),
   );
-  kataGroups.sortByNumberOfLinks();
-  kataGroups.moveGroupWithNewestKataToBeginning();
-  const allKataGroups = () => kataGroups.allGroups();
+  kataBundle.sortByNumberOfLinks();
+  kataBundle.moveGroupWithNewestKataToBeginning();
+  const allKataGroups = () => kataBundle.allGroups();
   it('first is the one with the newest kata inside', () => {
     assert.equal(allKataGroups()[0].name, 'group with newest kata');
   });
@@ -56,32 +56,32 @@ describe('sort kata groups', () => {
   });
 
   it('by name when number of files is the same', () => {
-    const kataGroups = new KataBundle();
-    kataGroups.addGroup(
+    const kataBundle = new KataBundle();
+    kataBundle.addGroup(
       KataGroup.fromRawKataItems('group b', [Kata.withId(0)]),
     );
-    kataGroups.addGroup(
+    kataBundle.addGroup(
       KataGroup.fromRawKataItems('group a', [Kata.withId(1)]),
     );
-    kataGroups.sortByNumberOfLinks();
-    kataGroups.moveGroupWithNewestKataToBeginning();
+    kataBundle.sortByNumberOfLinks();
+    kataBundle.moveGroupWithNewestKataToBeginning();
 
-    assert.equal(kataGroups.allGroups()[0].name, 'group a');
+    assert.equal(kataBundle.allGroups()[0].name, 'group a');
   });
 });
 
 describe('find newest kata', () => {
   it('the newest kata is the one with the highest ID', () => {
-    const kataGroups = new KataBundle();
-    kataGroups.addGroup(
+    const kataBundle = new KataBundle();
+    kataBundle.addGroup(
       KataGroup.fromRawKataItems('group with 1 kata', [Kata.withId(2)]),
     );
-    kataGroups.addGroup(
+    kataBundle.addGroup(
       KataGroup.fromRawKataItems('group with 2 katas', [
         Kata.withId(4),
         Kata.withId(13),
       ]),
     );
-    assert.equal(kataGroups.isNewestKata(Kata.withId(13)), true);
+    assert.equal(kataBundle.isNewestKata(Kata.withId(13)), true);
   });
 });
